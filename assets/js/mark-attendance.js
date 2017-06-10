1127f5f734aa6012beb0928d220234d168042135
asheesh <asheesh@ahex.co.in>
2017-06-10 13:33:18 +0530

    added student info page

--- assets/js/mark-attendance.js
+++ assets/js/mark-attendance.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     $(".mark-present").click(function () {
         $('.students-wrapper #schedulingAnnouncement').slideDown();
-        $('#announcementCheckbox').prop('checked');
+//        $(".students-wrapper #announcementCheckbox").trigger('change');
+        $(".students-wrapper #announcementCheckbox").prop('checked',true);
+        $(".students-wrapper .announcementCheck").prop('checked',true);
+        $(".students-wrapper .switch .cmn-toggle").prop('checked',true);
     });
     $(".mark-absent").click(function () {
         $('.students-wrapper #schedulingAnnouncement').slideUp();
-        $('#announcementCheckbox').prop('checked');
+//        $(".students-wrapper #announcementCheckbox").trigger('change');
+        $(".students-wrapper #announcementCheckbox").prop('checked',false);
+        $(".students-wrapper .announcementCheck").prop('checked',false);
+        $(".students-wrapper .switch .cmn-toggle").prop('checked',false);
+    });
+
+    /*--  On Click Row Redirecting to New Teacher Page  --*/
+    $('.students-wrapper .table-body .row .table-column').on('click', function () {
+        window.location.href = 'student-info.html';
     });
 });
 
